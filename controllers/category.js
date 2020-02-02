388497366299b484f60f50fe01e812b5e4524712
zonghua2016 <2280314971@qq.com>
2020-02-02 18:19:52 +0800

    将逻辑层和数据层导出方法改为类的静态方法

--- controllers/category.js
+++ controllers/category.js
@@ -2,7 +2,7 @@
  * @Description  : 分类控制器
  * @Author       : tongzonghua
  * @Date         : 2020-02-02 00:18:51
- * @LastEditTime : 2020-02-02 03:23:19
+ * @LastEditTime : 2020-02-02 16:09:14
  * @LastEditors  : tongzonghua
  * @Email        : tongzonghua@gmail.com
  * @GitHub       : https://github.com/zonghua2016
@@ -10,8 +10,8 @@
 
 const categoryService = require('../services').category;
 
-const categoryController = {
-    async getCategoryList(ctx, next) {
+class CategoryController  {
+    static async getCategoryList(ctx, next) {
         const categoryList = await categoryService.getCategoryList();
         ctx.result = {
             categoryList
@@ -20,4 +20,4 @@ const categoryController = {
     }
 }
 
-module.exports = categoryController;
+module.exports = CategoryController;
